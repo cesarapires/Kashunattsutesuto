ea7e4a147fa573ec33e8aa8b6f95f5ecd1395fbd
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-20 18:22:47 -0300

    feat: add spring-boot aplication

---
+++ src/main/java/com/kashunattsutesuto/KashunattsutesutoApplication.java
@@ -0,0 +1,13 @@
+package com.kashunattsutesuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashunattsutesutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashunattsutesutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashunattsutesuto/KashunattsutesutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashunattsutesuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashunattsutesutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
