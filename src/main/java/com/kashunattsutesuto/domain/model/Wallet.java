200c708ec5638a31d920183b630f64538a090f80
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-21 15:10:04 -0300

    feat: add model Wallet

---
+++ src/main/java/com/kashunattsutesuto/domain/model/Wallet.java
@@ -0,0 +1,16 @@
+package com.kashunattsutesuto.domain.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Wallet {
+    private String id;
+
+    private String accountId;
+
+    private BigDecimal foodBalance;
+
+    private BigDecimal mealBalance;
+
+    private BigDecimal cashBalance;
+}
