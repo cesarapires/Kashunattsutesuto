bbaa8bac8d389b0bbe1193082373124134cf7741
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-21 18:19:18 -0300

    feat: add method to create transaction in TransactionPersistenceAdapter

--- src/main/java/com/kashunattsutesuto/domain/contracts/repositories/TransactionPersistenceAdapter.java
+++ src/main/java/com/kashunattsutesuto/domain/contracts/repositories/TransactionPersistenceAdapter.java
@@ -1,4 +1,7 @@
 package com.kashunattsutesuto.domain.contracts.repositories;
 
+import com.kashunattsutesuto.domain.model.Transaction;
+
 public interface TransactionPersistenceAdapter {
+    Transaction save(Transaction transaction);
 }
