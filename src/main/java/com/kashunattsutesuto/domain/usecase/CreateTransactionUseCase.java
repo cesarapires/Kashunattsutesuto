cdcea4fb54e032ae29d0aea1472a904953fe8217
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-22 08:10:19 -0300

    feat: add persistence of wallet and transaction and calculates the debit wallet

--- src/main/java/com/kashunattsutesuto/domain/usecase/CreateTransactionUseCase.java
+++ src/main/java/com/kashunattsutesuto/domain/usecase/CreateTransactionUseCase.java
@@ -1,22 +1,76 @@
 package com.kashunattsutesuto.domain.usecase;
 
+import com.kashunattsutesuto.domain.contracts.repositories.TransactionPersistenceAdapter;
 import com.kashunattsutesuto.domain.contracts.repositories.WalletPersistenceAdapter;
 import com.kashunattsutesuto.domain.feature.CreateTransaction;
 import com.kashunattsutesuto.domain.model.Transaction;
 import com.kashunattsutesuto.domain.model.TransactionStatus;
 import com.kashunattsutesuto.domain.model.Wallet;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Service
 @AllArgsConstructor
+@Transactional
 public class CreateTransactionUseCase implements CreateTransaction {
 
     private final WalletPersistenceAdapter walletPersistenceAdapter;
+
+    private final TransactionPersistenceAdapter transactionPersistenceAdapter;
 
     @Override
     public TransactionStatus create(Transaction transaction) {
        Wallet wallet = walletPersistenceAdapter.getWalletByAccountId(transaction.getAccountId());
        if (wallet == null) return TransactionStatus.GENERIC_ERROR;
 
+       BigDecimal amount = transaction.getAmount();
+
+       switch (transaction.getMccCategory()) {
+           case MEAL:
+               BigDecimal mealBalance = wallet.getMealBalance();
+               if (!validatesBalanceIsSufficient(mealBalance, amount)) {
+                   amount = adjustAmountFromCashBalance(wallet, amount, mealBalance);
+                   if (amount == null) return TransactionStatus.REJECTED_INSUFFICIENT_BALANCE;
+               }
+               wallet.setMealBalance(mealBalance.subtract(amount));
+               break;
+           case FOOD:
+               BigDecimal foodBalance = wallet.getFoodBalance();
+               if (!validatesBalanceIsSufficient(foodBalance, amount)) {
+                   amount = adjustAmountFromCashBalance(wallet, amount, foodBalance);
+                   if (amount == null) return TransactionStatus.REJECTED_INSUFFICIENT_BALANCE;
+               }
+               wallet.setFoodBalance(foodBalance.subtract(amount));
+               break;
+           default:
+               BigDecimal cashBalance = wallet.getCashBalance();
+               if (!validatesBalanceIsSufficient(cashBalance, amount)) return TransactionStatus.REJECTED_INSUFFICIENT_BALANCE;
+               wallet.setCashBalance(cashBalance.subtract(amount));
+               break;
+       }
+
+
+       walletPersistenceAdapter.save(wallet);
+       transactionPersistenceAdapter.save(transaction);
+
        return TransactionStatus.SUCCESS;
+    }
+
+    private BigDecimal adjustAmountFromCashBalance(Wallet wallet, BigDecimal amount, BigDecimal foodBalance) {
+        BigDecimal cashBalance = wallet.getCashBalance();
+        BigDecimal balanceRequired = amount.subtract(foodBalance);
+
+        if (!validatesBalanceIsSufficient(cashBalance, balanceRequired)) return null;
+
+        wallet.setCashBalance(cashBalance.subtract(balanceRequired));
+
+        return amount.subtract(balanceRequired);
+    }
+
+    private Boolean validatesBalanceIsSufficient(BigDecimal walletBalance, BigDecimal amount) {
+        return walletBalance.compareTo(amount) >= 0;
     }
 }
--- src/test/java/com/kashunattsutesuto/domain/usecase/CreateTransactionUseCaseTest.java
+++ src/test/java/com/kashunattsutesuto/domain/usecase/CreateTransactionUseCaseTest.java
@@ -1,9 +1,11 @@
 package com.kashunattsutesuto.domain.usecase;
 
+import com.kashunattsutesuto.domain.contracts.repositories.TransactionPersistenceAdapter;
 import com.kashunattsutesuto.domain.contracts.repositories.WalletPersistenceAdapter;
 import com.kashunattsutesuto.domain.model.MCCCategory;
 import com.kashunattsutesuto.domain.model.Transaction;
 import com.kashunattsutesuto.domain.model.TransactionStatus;
+import com.kashunattsutesuto.domain.model.Wallet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,32 +22,126 @@ class CreateTransactionUseCaseTest {
 
     @Mock WalletPersistenceAdapter walletPersistenceAdapter;
 
+    @Mock TransactionPersistenceAdapter transactionPersistenceAdapter;
+
     CreateTransactionUseCase createTransactionUseCase;
 
     private Transaction transaction;
 
     @BeforeEach
     public void setUp() {
-        createTransactionUseCase = new CreateTransactionUseCase(walletPersistenceAdapter);
+        createTransactionUseCase = new CreateTransactionUseCase(walletPersistenceAdapter, transactionPersistenceAdapter);
         transaction = new Transaction(
                 "123456",
                 "123",
                 new BigDecimal(45.00),
                 "PADARIA DO ZE SAO PAULO BR",
-                MCCCategory.MEAL
+                MCCCategory.MEAL,
+                1
         );
+    }
+
+    @Test
+    public void shouldCallTransactionPersistenceWithCorrectParams() {
+        // Arrange
+        String accountId = "123";
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(0), new BigDecimal(45), new BigDecimal(0), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        createTransactionUseCase.create(transaction);
+
+        // Assert
+        verify(transactionPersistenceAdapter).save(transaction);
     }
 
     @Test
     public void shouldCallWalletPersistenceWithCorrectParams() {
         // Arrange
         String accountId = "123";
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(0), new BigDecimal(45), new BigDecimal(0), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
 
         // Act
         createTransactionUseCase.create(transaction);
 
         // Assert
-        verify(walletPersistenceAdapter).getWalletByAccountId(accountId);
+        wallet.setMealBalance(new BigDecimal(0));
+        verify(walletPersistenceAdapter).save(wallet);
+    }
+
+    @Test
+    public void shouldRejectTransactionDueToInsufficientMealBalance() {
+        // Arrange
+        String accountId = "123";
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(100), new BigDecimal(44.99), new BigDecimal(0), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        TransactionStatus result = createTransactionUseCase.create(transaction);
+
+        // Assert
+        assertEquals(result, TransactionStatus.REJECTED_INSUFFICIENT_BALANCE);
+    }
+
+    @Test
+    public void shouldRejectTransactionDueToInsufficientFoodBalance() {
+        // Arrange
+        String accountId = "123";
+        transaction.setMccCategory(MCCCategory.FOOD);
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(44.99), new BigDecimal(100), new BigDecimal(0), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        TransactionStatus result = createTransactionUseCase.create(transaction);
+
+        // Assert
+        assertEquals(result, TransactionStatus.REJECTED_INSUFFICIENT_BALANCE);
+    }
+
+    @Test
+    public void shouldCreateTransactionWithCashAndFoodBalance() {
+        // Arrange
+        String accountId = "123";
+        transaction.setMccCategory(MCCCategory.FOOD);
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(20), new BigDecimal(100), new BigDecimal(25), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        TransactionStatus result = createTransactionUseCase.create(transaction);
+
+        // Assert
+        assertEquals(result, TransactionStatus.SUCCESS);
+    }
+
+    @Test
+    public void shouldCreateTransactionWithCashAndMealBalance() {
+        // Arrange
+        String accountId = "123";
+        transaction.setMccCategory(MCCCategory.MEAL);
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(100), new BigDecimal(20), new BigDecimal(25), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        TransactionStatus result = createTransactionUseCase.create(transaction);
+
+        // Assert
+        assertEquals(result, TransactionStatus.SUCCESS);
+    }
+
+    @Test
+    public void shouldRejectTransactionDueToInsufficientCashBalance() {
+        // Arrange
+        String accountId = "123";
+        transaction.setMccCategory(MCCCategory.OTHERS);
+        Wallet wallet = new Wallet("12345", accountId, new BigDecimal(0), new BigDecimal(0), new BigDecimal(44.99), 1);
+        when(walletPersistenceAdapter.getWalletByAccountId(accountId)).thenReturn(wallet);
+
+        // Act
+        TransactionStatus result = createTransactionUseCase.create(transaction);
+
+        // Assert
+        assertEquals(result, TransactionStatus.REJECTED_INSUFFICIENT_BALANCE);
     }
 
     @Test
