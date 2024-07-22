fd21e93473a227d2bf897cbf42a4b451dc6b3892
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-22 08:07:11 -0300

    feat: add method to find wallet by accountId

--- src/main/java/com/kashunattsutesuto/infraestructure/repositories/postgres/repository/WalletRepository.java
+++ src/main/java/com/kashunattsutesuto/infraestructure/repositories/postgres/repository/WalletRepository.java
@@ -2,8 +2,14 @@ package com.kashunattsutesuto.infraestructure.repositories.postgres.repository;
 
 import com.kashunattsutesuto.infraestructure.repositories.postgres.entities.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository <WalletEntity, String> {
+
+    Optional<WalletEntity> findByAccountId(@Param("account_id") String accountId);
+
 }
