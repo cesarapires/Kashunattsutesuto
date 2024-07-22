83990bfccd7a8e01aa155529eeec0829f5af2e4c
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-07-22 08:10:45 -0300

    feat: add REJECTED_INSUFFICIENT_BALANCE in TransactionStatus

--- src/main/java/com/kashunattsutesuto/domain/model/TransactionStatus.java
+++ src/main/java/com/kashunattsutesuto/domain/model/TransactionStatus.java
@@ -2,6 +2,7 @@ package com.kashunattsutesuto.domain.model;
 
 public enum TransactionStatus {
     GENERIC_ERROR("07"),
+    REJECTED_INSUFFICIENT_BALANCE("51"),
     SUCCESS("00");
 
     private String code;
